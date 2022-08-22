d32d0dc51aa31469d85984c12ce226dd91a0e000
wassy310 <ryg.wsza@gmail.com>
2022-08-22 21:44:36 +0900

    improve readability

--- utils/draw.py
+++ utils/draw.py
@@ -228,16 +228,16 @@ def draw_jp_brect(image, brect, hand_sign_text):
     return image
 
 def draw_info(image, fps, mode, number):
-    cv.putText(image, "FPS:" + str(fps), (10, 30), cv.FONT_HERSHEY_SIMPLEX,
+    cv.putText(image, "FPS: " + str(fps), (10, 30), cv.FONT_HERSHEY_SIMPLEX,
                1.0, (0, 0, 0), 4, cv.LINE_AA)
-    cv.putText(image, "FPS:" + str(fps), (10, 30), cv.FONT_HERSHEY_SIMPLEX,
+    cv.putText(image, "FPS: " + str(fps), (10, 30), cv.FONT_HERSHEY_SIMPLEX,
                1.0, (255, 255, 255), 2, cv.LINE_AA)
 
     if 0 <= mode <= 9:
-        cv.putText(image, "MODE:" + str(mode), (10, 90),
+        cv.putText(image, "MODE: " + str(mode), (10, 90),
                    cv.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1, cv.LINE_AA)
         if 0 <= number <= 9:
-            cv.putText(image, "NUM:" + str(number), (10, 110),
+            cv.putText(image, "NUM: " + str(number), (10, 110),
                        cv.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1, cv.LINE_AA)
 
     return image

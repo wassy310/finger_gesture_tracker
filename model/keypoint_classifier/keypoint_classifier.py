967bc1055c14d8bdae2d247a801880ad74b0447b
wassy310 <ryg.wsza@gmail.com>
2022-09-18 15:14:00 +0900

    update file

--- model/keypoint_classifier/keypoint_classifier.py
+++ model/keypoint_classifier/keypoint_classifier.py
@@ -1,18 +1,16 @@
-import numpy as np
-import tensorflow as tf
+import numpy as np, tensorflow as tf
 
 class KeyPointClassifier(object):
-    def __init__(self,model_path, num_threads = 1, ):
-        self.interpreter = tf.lite.Interpreter(model_path = model_path,
-                                               num_threads = num_threads)
+    def __init__(self, model_path, num_threads = 1, ):
+        self.interpreter = tf.lite.Interpreter(
+            model_path = model_path,
+            num_threads = num_threads
+            )
         self.interpreter.allocate_tensors()
         self.input_details = self.interpreter.get_input_details()
         self.output_details = self.interpreter.get_output_details()
 
-    def __call__(
-        self,
-        landmark_list,
-    ):
+    def __call__(self, landmark_list, ):
         input_details_tensor_index = self.input_details[0]['index']
         self.interpreter.set_tensor(
             input_details_tensor_index,
